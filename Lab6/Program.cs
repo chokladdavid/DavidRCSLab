0ab2c741070d32b9edcff541b56ddc512faa87c5
chokladdavid <david.ramvall@comhem.se>
2015-12-16 13:16:51 +0100

    2 klar med komentarer

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -17,7 +17,17 @@ namespace Lab6
             ulong myUlong = 567UL;
 
             int tal1 = 7, tal2 = 13;
-
+            //Console.WriteLine(Convert.ToString(tal1 & tal2));
+            
+            Console.WriteLine($"AND:{tal1 & tal2} OR:{tal1 | tal2} XOR:{tal1 ^ tal2}");
+            // gissning?   rätt!        7? 5!            6? 15!            10?!
+            /*
+                                    0111    7           0111    7           0111    7           
+                                    1101    13          1101    13          1101    13          
+                                    --------------      --------------      --------------      
+                                    0101    7 & 13      1111    7 | 13      1010    7 ^ 13      
+                                    om båda true        om någon true       om bara en e true
+            */
 
             Point point = new Point(2, 3);
             Point3D point3D = point;
@@ -28,7 +38,7 @@ namespace Lab6
             MyDoubleType mtd4 = new MyDoubleType(7.7);
 
             Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}",
-                mtd1, mtd2, mtd3, mtd1 == mtd4, mtd4 - mtd1, mtd1 + mtd2 - mtd3 - mtd4, mtd1 < mtd4);
+                mtd1, mtd2, mtd3, mtd1 == mtd4, mtd4 - mtd1, mtd1 + mtd2 - mtd3 / mtd4, mtd1 < mtd4);
         }
     }
 }
